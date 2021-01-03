483182e794fd550fea997d95b0805693b8c2cec3
akinhet <piti200406@gmail.com>
2021-01-03 01:09:42 +0100

    added screenshot.sh

--- color-copy.sh
+++ color-copy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 # https://github.com/Jack12816/colorpicker.git
+# https://github.com/astrand/xclip.git
+
 colorpicker --short --one-shot | xclip -selection clipboard
---
+++ screenshot.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# https://github.com/naelstrof/maim.git
+# https://github.com/astrand/xclip.git
+
+maim -s | xclip -selection clipboard -t image/png
